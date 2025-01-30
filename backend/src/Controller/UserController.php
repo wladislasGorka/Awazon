4377fb486968d479714c63f4e166c6bfd080083f
wgorka <wladislas.gorka@gmail.com>
2025-01-30 23:49:12 +0100

    user creation

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        // Récupérer les données de la requête
+        $data = json_decode($request->getContent(), true);
+
+        // Vérifier que les données nécessaires sont présentes
+        if (!isset($data['name'], $data['email'], $data['password'])) {
+            return new JsonResponse(['error' => 'Invalid data'], 400);
+        }
+
+        // Créer un nouvel utilisateur et définir ses propriétés
+        $user = new User();
+        $user->setName($data['name']);
+        $user->setMail($data['email']);
+        $user->setPassword($data['password']);
+
+        // Enregistrer l'utilisateur dans la base de données
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Envoyer une réponse JSON
+        return new JsonResponse(['message' => 'User created successfully', 'userId' => $user->getId()], 201);
     }
 }
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,18 +2,23 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <v-container>
+        <user-form></user-form>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UserForm from './components/UserForm.vue';
 
 export default {
   name: 'App',
-
+  
   components: {
     HelloWorld,
+    UserForm
   },
 
   data: () => ({
